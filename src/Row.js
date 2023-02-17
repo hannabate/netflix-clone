b0acdd5b977f0c4ddb0f72a15f04b464ebeb4aad
hannabate <hmengesha2001@gmail.com>
2023-02-16 21:36:33 -0500

    build

---
+++ src/Row.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+import "./Row.css";
+import axios from "./axios";
+import youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
+
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, largeRow }) {
+  const [movies, setmovies] = useState([]);
+  const [traileUrl, setTraileUrl] = useState("");
+  
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      // console.log(request)
+      setmovies(request.data.results);
+      // console.log(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (traileUrl) {
+      setTraileUrl ("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie.original_name)
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTraileUrl(urlParams.get("v"))
+        })
+        .catch((error) => console.log(error));
+    }
+  }
+
+  // console.log(movies)
+
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            onClick={()=> handleClick(movie)}
+            className={`row__poster ${largeRow && "row__posterLarge"}`}
+            src={`${base_url}${ largeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      <div style={{ padding: "40px" }}>
+        {traileUrl && <YouTube videoId= {traileUrl} opts={opts}/>}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
---
+++ src/requests.js
@@ -0,0 +1,16 @@
+// my api key
+// 3c177ae0747ffe658b80b2cdeea74dd8
+const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = "226a392e3ed2d3ae6de92b85a1074c73";
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
+
